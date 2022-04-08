9b41155b053aecd7a3ef630984281920b1586304
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2022-04-08 16:36:12 +0300

    Add files via upload

---
+++ movies_web_spa/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="text-decoration-underline text-center text-red">TEST</h1>
+    <v-btn depressed color="primary" class=""> Primary </v-btn>
+    <div class="mx-auto text-center">
+      <v-icon icon="mdi-home"/>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+
+<style>
+h1 {
+  margin: 3em;
+}
+</style>
